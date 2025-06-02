31f93850f35db1dfb9f672b261180215b0d4fd77
tt1125 <tsurutsuru275@gmail.com>
2025-06-03 04:05:56 +0900

    feat: AppコンポーネントにAPIリンクを追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +27,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        <a href="/api/hello">API</a>
       </p>
     </>
   )
