0dab04dfe61022c8c2ba67b5813b1b604d2858c0
tt1125 <tsurutsuru275@gmail.com>
2025-06-03 04:06:39 +0900

    feat: FastAPIアプリケーションにReactの静的ファイルをマウント

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api/hello")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+app.mount("/", StaticFiles(directory="server/dist", html=True), name="static")
